67036356377059afb562758f880f12519b2eeb3a
jon <jonathanerhine@gmail.com>
2026-03-11 21:26:04 -0500

    push error

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+
+namespace QcOnLocation.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AuthController : Controller
+{
+    [HttpGet("login")]
+    public IActionResult Login([FromQuery] string? returnUrl = "/locations")
+    {
+        var props = new AuthenticationProperties { RedirectUri = returnUrl };
+        return Challenge(props, "Google");
+    }
+    //push
+}
+
+
+
+
